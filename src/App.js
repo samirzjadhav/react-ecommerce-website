9166c93377f0869b74c6f341378839bd0ed8e2f2
Samir Jadhav <samirzjadhav@gmail.com>
2024-05-23 14:00:00 +0100

    feat: added navbar header head and search files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Header from "./Common/Header/Header";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Common/Header/Head.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
+const Head = () => {
+  return (
+    <>
+      <section className="head">
+        <div className="container flex">
+          <div className="left row flex justify-center items-center">
+            <i>
+              <FaPhoneAlt />
+            </i>
+            <label>+88034 4444 4444</label>
+            <i>
+              <MdEmail />
+            </i>
+            <label>example@gmail.com</label>
+          </div>
+          <div className="right row RTtext flex justify-center items-center gap-[4px]">
+            <label>Theme FAQ's</label>
+            <label>Need Helps</label>
+            <span>🇮🇳</span>
+            <label htmlFor="">INR</label>
+            <span>🇺🇸</span>
+            <label htmlFor="">USD</label>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Head;
