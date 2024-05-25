f141d647d162ab43d565a13f849209395d224f6b
Samir Jadhav <samirzjadhav@gmail.com>
2024-05-25 13:08:35 -0700

    feat: added navbar

--- src/Common/Header/Navbar.js
+++ src/Common/Header/Navbar.js
@@ -1,9 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-      <h1>Navbar</h1>
+      <header className="header">
+        <div className="container d-flex">
+          <div className="categories d-flex">
+            <span></span>
+            <h4>
+              Categories <i></i>
+            </h4>
+          </div>
+          <div className="navlink"></div>
+          <ul className="nav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/pages">pages</Link>
+            </li>
+            <li>
+              <Link to="/user">user account</Link>
+            </li>
+            <li>
+              <Link to="/vender">vendor account</Link>
+            </li>
+            <li>
+              <Link to="/track">Track my order</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact us</Link>
+            </li>
+          </ul>
+        </div>
+      </header>
     </>
   );
 };
