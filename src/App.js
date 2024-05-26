5c5c0e1653a61e72b40efb801ac646aca8e4431e
Samir Jadhav <samirzjadhav@gmail.com>
2024-05-26 12:29:37 -0700

    feat: changes on files and packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Header from "./Common/Header/Header";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
     <>
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact></Route>
+        </Switch>
+      </Router>
     </>
   );
 }
--- src/Common/Header/Navbar.js
+++ src/Common/Header/Navbar.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [MobileMenu, setMobileMenu] = useState();
+
   return (
     <>
       <header className="header">
@@ -12,27 +14,38 @@ const Navbar = () => {
               Categories <i></i>
             </h4>
           </div>
-          <div className="navlink"></div>
-          <ul className="nav">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/pages">pages</Link>
-            </li>
-            <li>
-              <Link to="/user">user account</Link>
-            </li>
-            <li>
-              <Link to="/vender">vendor account</Link>
-            </li>
-            <li>
-              <Link to="/track">Track my order</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact us</Link>
-            </li>
-          </ul>
+          <div className="navlink">
+            <ul className="nav">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/pages">pages</Link>
+              </li>
+              <li>
+                <Link to="/user">user account</Link>
+              </li>
+              <li>
+                <Link to="/vender">vendor account</Link>
+              </li>
+              <li>
+                <Link to="/track">Track my order</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact us</Link>
+              </li>
+            </ul>
+            <button
+              className="toggle"
+              onClick={() => setMobileMenu(!MobileMenu)}
+            >
+              {MobileMenu ? (
+                <i className="close home-btn"></i>
+              ) : (
+                <i className="open"></i>
+              )}
+            </button>
+          </div>
         </div>
       </header>
     </>
