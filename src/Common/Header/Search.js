31944208a6a3ae6f1b03fd9ecf78eec7aefb3eec
Samir Jadhav <samirzjadhav@gmail.com>
2024-05-28 13:30:17 -0700

    search box added

--- src/Common/Header/Search.js
+++ src/Common/Header/Search.js
@@ -1,9 +1,40 @@
 import React from "react";
+import Logo from "./logo.svg";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { FaSearch } from "react-icons/fa";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { HiShoppingBag } from "react-icons/hi";
 
 const Search = () => {
   return (
     <>
-      <h1>Search</h1>
+      <section className="search">
+        <div className="container c-flex">
+          {" "}
+          <div className="logo width">
+            <img src={Logo} alt="" />
+          </div>
+          <div className="search-box flex">
+            <i>
+              <FaSearch />
+            </i>
+            <input type="text" placeholder="Search and hit enter" />
+            <span>All category</span>
+          </div>
+          <div className="icon flex width">
+            <i>
+              <FaRegCircleUser />
+            </i>
+            <div className="cart"></div>
+            <Link to="/cart">
+              <i>
+                <HiShoppingBag />
+              </i>
+              <span>0</span>
+            </Link>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
