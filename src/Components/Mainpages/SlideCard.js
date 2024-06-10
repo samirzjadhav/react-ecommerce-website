0c5fff1d8cd0989d353568e2cf57ddfe354536a4
Samir Jadhav <samirzjadhav@gmail.com>
2024-06-10 11:17:48 +0530

    fix: changes on slidercard

--- src/Components/Mainpages/SlideCard.js
+++ src/Components/Mainpages/SlideCard.js
@@ -1,20 +1,78 @@
+// import React from "react";
+// import Sdata from "./Sdata";
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
+// const SlideCard = () => {
+//   const settings = {
+//     dots: true,
+//     infinite: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     appendDots: (dots) => {
+//       return <ul style={{ margin: "0px" }}>{dots}</ul>;
+//     },
+//   };
+
+//   return (
+//     <>
+//       <Slider {...settings}>
+//         {Sdata.map((value, index) => {
+//           return (
+//             <div className="box flex top" key={index}>
+//               <div className="left">
+//                 <h1>{value.title}</h1>
+//                 <p>{value.desc}</p>
+//                 <button className="btn-primary">Visit Collections</button>
+//               </div>
+//               <div className="right">
+//                 <img src={value.cover} alt="" />
+//               </div>
+//             </div>
+//           );
+//         })}
+//       </Slider>
+//     </>
+//   );
+// };
+
+// export default SlideCard;
+
 import React from "react";
-import Sdata from "./Sdata.js";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const SlideCard = () => {
+export default function SimpleSlider() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
-    <>
-      {Sdata.map((value, index) => {
-        return (
-          <div className="box flex top">
-            <div className="left">
-              <h1>{value.title}</h1>
-            </div>
-          </div>
-        );
-      })}
-    </>
+    <Slider {...settings}>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider>
   );
-};
-
-export default SlideCard;
+}
