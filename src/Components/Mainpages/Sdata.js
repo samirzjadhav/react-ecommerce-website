97f08b1ab6a87d45457116c039dcda67ea7ff4d0
Samir Jadhav <samirzjadhav@gmail.com>
2024-06-15 13:00:07 +0530

    feat: changes s data

--- src/Components/Mainpages/Sdata.js
+++ src/Components/Mainpages/Sdata.js
@@ -1,26 +1,26 @@
 const Sdata = [
   {
     id: 1,
-    title: "50% Off For Your First Shopping",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
+    title: "Exclusive 60% Off on First Shoe Purchase",
+    desc: "Comfortable, durable, stylish shoes with high-quality materials, ergonomic designs, and advanced cushioning for various activities.",
     cover: "./images/SlideCard/slide-1.jpg",
   },
   {
     id: 2,
-    title: "50% Off For Your First Shopping",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
+    title: "40% Discount on Your First Watch Purchase",
+    desc: "Versatile timepieces with features like alarms, fitness tracking, notifications, and sleek designs for daily wear and sports.",
     cover: "./images/SlideCard/slide-2.jpg",
   },
   {
     id: 3,
-    title: "50% Off For Your First Shopping",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
+    title: "Save 70% on Your First Shirt Purchase",
+    desc: "High-quality, comfortable shirts in various styles and fits for casual, formal, or everyday wear.",
     cover: "./images/SlideCard/slide-3.jpg",
   },
   {
     id: 4,
-    title: "50% Off For Your First Shopping",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
+    title: "Get 80% Off on Your First Bag Purchase",
+    desc: "Stylish, practical bags with multiple compartments and durable materials for everyday use and travel.",
     cover: "./images/SlideCard/slide-4.jpg",
   },
 ];
