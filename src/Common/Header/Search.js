1e4afafd91376c84a82186d9189465a49e3645a9
samir jadhav <samirzjadhav@gmail.com>
2024-07-01 14:02:02 +0530

    fix: scroll event on search bar

--- src/Common/Header/Search.js
+++ src/Common/Header/Search.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Logo from "./logo.svg";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { FaSearch } from "react-icons/fa";
@@ -6,41 +6,50 @@ import { FaRegCircleUser } from "react-icons/fa6";
 import { HiShoppingBag } from "react-icons/hi";
 
 const Search = ({ cartItem }) => {
-  window.addEventListener("scroll", () => {
-    const search = document.querySelector(".search");
-    search.classList.toggle("active", window.scrollY > 100);
-  });
+  useEffect(() => {
+    const handleScroll = () => {
+      const search = document.querySelector(".search");
+      if (search) {
+        search.classList.toggle("active", window.scrollY > 100);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <>
-      <section className="search">
-        <div className="container c-flex">
-          {" "}
-          <div className="logo width">
-            <img src={Logo} alt="" />
-          </div>
-          <div className="search-box flex">
-            <i>
-              <FaSearch />
-            </i>
-            <input type="text" placeholder="Search and hit enter" />
-            <span>All category</span>
-          </div>
-          <div className="icon flex width">
-            <i className="icon-circle flex justify-center items-center">
-              <FaRegCircleUser />
-            </i>
-            <div className="cart w-[50px] flex justify-center items-center icon-circle">
-              <Link to="/cart">
-                <i>
-                  <HiShoppingBag />
-                </i>
-                <span>{cartItem.length === 0 ? "" : cartItem.length}</span>
-              </Link>
-            </div>
+    <section className="search">
+      <div className="container c-flex">
+        <div className="logo width">
+          <img src={Logo} alt="Logo" />
+        </div>
+        <div className="search-box flex">
+          <i>
+            <FaSearch />
+          </i>
+          <input type="text" placeholder="Search and hit enter" />
+          <span>All category</span>
+        </div>
+        <div className="icon flex width">
+          <i className="icon-circle flex justify-center items-center">
+            <FaRegCircleUser />
+          </i>
+          <div className="cart w-[50px] flex justify-center items-center icon-circle">
+            <Link to="/cart">
+              <i>
+                <HiShoppingBag />
+              </i>
+              <span>{cartItem.length === 0 ? "" : cartItem.length}</span>
+            </Link>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
